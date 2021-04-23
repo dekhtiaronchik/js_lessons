4b3852b6caec3ad6da56852391c76a9ddf1b00ae
dekhtiaronchik <72033715+dekhtiaronchik@users.noreply.github.com>
2021-04-23 18:25:03 +0200

    lesson_adv2 tasks (#9)

---
+++ internet_shop/js/cartItem.js
@@ -0,0 +1,18 @@
+class CartItem {
+
+    constructor(product, amount, discount) {
+        this.product = product;
+        this.amount = amount;
+        this.discount = discount;
+    }
+
+
+    render() {
+
+    }
+
+    //считает стоимость товара с учетем его количества и скидки
+    getTotalPrice() {
+
+    }
+}
