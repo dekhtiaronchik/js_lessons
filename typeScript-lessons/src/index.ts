89b0f71c947087eef76f7124bfda937a398285b0
dekhtiaronchik <tatiana.dekhtiaronok@gmail.com>
2021-10-26 18:52:58 +0200

    lesson1

---
+++ typeScript-lessons/src/index.ts
@@ -0,0 +1,22 @@
+import { renderSearchFormBlock } from './search-form.js';
+import { renderSearchStubBlock } from './search-results.js';
+import { renderUserBlock } from './user.js';
+import { renderToast } from './lib.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('Ann Smith', '/img/avatar.png', 3);
+  renderSearchFormBlock('19-11-2021', '25-11-2021');
+  renderSearchStubBlock();
+  renderToast(
+    {
+      text: 'Это пример уведомления. Используйте его при необходимости',
+      type: 'success',
+    },
+    {
+      name: 'Понял',
+      handler: () => {
+        console.log('Уведомление закрыто');
+      },
+    }
+  );
+});
